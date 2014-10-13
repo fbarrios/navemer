d6b777d6fcce1703cedaa3c178e3792624ab369a
Federico Barrios <fbarrios@fi.uba.ar>
2014-10-13 20:06:43 -0300

    Messing around with events.

--- GUI_Research/map.py
+++ GUI_Research/map.py
@@ -11,12 +11,17 @@ def main():
     background = pygame.image.load('background.jpg').convert()
     screen.blit(background, (0, 0))
 
-    pygame.draw.line(screen, color_white, (60, 10), (400, 400), 4)
-
     while True:
         for event in pygame.event.get():
-            if event.type in (QUIT, KEYDOWN):
+            if event.type == QUIT:
                 return
+
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                return
+
+            elif event.type == MOUSEBUTTONDOWN:
+                x, y = event.pos
+                pygame.draw.line(screen, color_white, (320,240), (x, y), 2)
 
         pygame.display.update()
         pygame.time.delay(10)
