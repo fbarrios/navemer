2238f2c838939adc6f9462ff4c6111911c4f371a
ezetowers <ezequiel.torresfeyuk@gmail.com>
2014-11-06 02:55:47 -0300

    * Set a interface for the emergency vehicules to guide. No funcionality has been developed yet

--- navigation/map.py
+++ navigation/map.py
@@ -20,16 +20,64 @@ from gi.repository import GdkX11
 # Miscellaneous constants.
 MAP_FILE = "city/map.png"
 WINDOW_SIZE = 1006, 397
-WINDOW_CAPTION = "Sistema de Navegacion para Emergencias"
+WINDOW_CAPTION = "NAVEMER - Sistema de Navegacion para Emergencias (Interfaz Vehículo)"
 DEFAULT_LINE_COLOR = pygame.Color(70, 70, 125)
 
 class GameWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self)
-        vbox = Gtk.VBox(False, 2)
-        vbox.show()
-        self.add(vbox)
+        self.vbox = Gtk.VBox(False, 5)
+        self.vbox.show()
 
+        self.set_title(WINDOW_CAPTION)
+        self.add(self.vbox)
+
+        # Initialize the City backend part (Must be created before the Drawing Area)
+        self.city = City()
+        self.storage = Drawable_Object_Storage()
+        self.init_point = None
+        self.end_point = None
+
+        self.create_window_interface()
+
+        # Add some buttons
+        '''
+        hbox = Gtk.HBox(2, False)
+        combo = Gtk.ComboBoxText()
+        combo.append_text("BIRD")
+        combo.append_text("IS")
+        combo.set_active(0)
+        vbox.pack_end(hbox, False, False, 0)
+
+        self.button = Gtk.Button("Toggle Draw Area")
+        self.button.connect("clicked", self.button_clicked, None)
+        hbox.pack_end(combo, False, False, 0)
+        hbox.pack_end(self.button, False, False, 0)
+        hbox.show_all()
+        '''
+    
+
+    def create_window_interface(self):
+        # Zone 1
+        self.create_menu()
+
+        # Zone 2 
+        # TODO
+
+        # Zone 3
+        self.create_map()
+
+        # Zone 4
+        self.create_origin_block_interface()
+
+        # Zone 5
+        self.create_destination_block_interface()
+
+        # Zone 6
+        self.create_toggle_button()
+
+
+    def create_menu(self):
         #create the menu
         file_menu = Gtk.Menu()
 
@@ -51,7 +99,7 @@ class GameWindow(Gtk.Window):
         quit_item.show()
 
         menu_bar = Gtk.MenuBar()
-        vbox.pack_start(menu_bar, False, False, 0)
+        self.vbox.pack_start(menu_bar, False, False, 0)
         menu_bar.show()
 
         file_item = Gtk.MenuItem()
@@ -62,23 +110,123 @@ class GameWindow(Gtk.Window):
         file_item.set_submenu(file_menu)
         menu_bar.append(file_item)
 
-        # City part (Must be created before the Drawing Area)
-        self.city = City()
-        self.storage = Drawable_Object_Storage()
-        self.init_point = None
-        self.end_point = None
-        
+
+    def create_map(self):
         #create the drawing area
         da = Gtk.DrawingArea()
         da.set_size_request(WINDOW_SIZE[0], WINDOW_SIZE[1])
-        vbox.pack_end(da, False, False, 0)
+        self.vbox.pack_start(da, False, False, 0)
         da.connect("realize", self._realized)
         da.show()
 
-        #collect key press events
-        self.connect("key-press-event", self.key_pressed)
+
+    def create_origin_block_interface(self):
+        vbox = Gtk.VBox(False, 5)
+        self.vbox.pack_start(vbox, False, False, 0)
+
+        hseparator = Gtk.HSeparator()
+        main_label = Gtk.Label()
+        hbox_main_label = Gtk.HBox(False, 5)
+        main_label.set_text("Ingrese dirección de origen:")
+        main_label.set_use_underline(True)
+        hbox_main_label.pack_start(main_label, False, False, 5)
+
+        # Street 1
+        hbox_street_1 = Gtk.HBox(False, 5)
+        label_street_1 = Gtk.Label()
+        label_street_1.set_text("Calle Origen 1:   ")
+
+        cb_street_1 = Gtk.ComboBoxText()
+        cb_street_1.append_text("Campillo")
+        cb_street_1.set_active(0)
+        hbox_street_1.pack_start(label_street_1, False, False, 5)
+        hbox_street_1.pack_start(cb_street_1, False, False, 0)
+
+        # Street 2
+        hbox_street_2 = Gtk.HBox(False, 5)
+        label_street_2 = Gtk.Label()
+        label_street_2.set_text("Calle Origen 2:   ")
+
+        cb_street_2 = Gtk.ComboBoxText()
+        cb_street_2.append_text("Torrent")
+        cb_street_2.set_active(0)
+        hbox_street_2.pack_start(label_street_2, False, False, 5)
+        hbox_street_2.pack_start(cb_street_2, False, False, 0)
+
+        # Add the streets to the container
+        vbox.pack_start(hseparator, False, False, 0)
+        vbox.pack_start(hbox_main_label, False, False, 0)
+        vbox.pack_start(hbox_street_1, False, False, 0)
+        vbox.pack_start(hbox_street_2, False, False, 0)
+        vbox.show_all()
 
 
+    def create_destination_block_interface(self):
+        vbox = Gtk.VBox(False, 5)
+        self.vbox.pack_start(vbox, False, False, 0)
+
+        hseparator = Gtk.HSeparator()
+        main_label = Gtk.Label()
+        hbox_main_label = Gtk.HBox(False, 5)
+        main_label.set_text("Ingrese dirección de destino:")
+        main_label.set_use_underline(True)
+        hbox_main_label.pack_start(main_label, False, False, 5)
+
+        # Street 1
+        hbox_street_1 = Gtk.HBox(False, 5)
+        label_street_1 = Gtk.Label()
+        label_street_1.set_text("Calle Destino 1:   ")
+
+        cb_street_1 = Gtk.ComboBoxText()
+        cb_street_1.append_text("Avenida Paseo Colon")
+        cb_street_1.set_active(0)
+        hbox_street_1.pack_start(label_street_1, False, False, 5)
+        hbox_street_1.pack_start(cb_street_1, False, False, 0)
+
+        # Street 2
+        hbox_street_2 = Gtk.HBox(False, 5)
+        label_street_2 = Gtk.Label()
+        label_street_2.set_text("Calle Destino 2:   ")
+
+        cb_street_2 = Gtk.ComboBoxText()
+        cb_street_2.append_text("Avenida Independencia")
+        cb_street_2.set_active(0)
+        hbox_street_2.pack_start(label_street_2, False, False, 5)
+        hbox_street_2.pack_start(cb_street_2, False, False, 0)
+
+        # Add the streets to the container
+        vbox.pack_start(hseparator, False, False, 0)
+        vbox.pack_start(hbox_main_label, False, False, 0)
+        vbox.pack_start(hbox_street_1, False, False, 0)
+        vbox.pack_start(hbox_street_2, False, False, 0)
+        vbox.show_all()
+
+
+    def create_toggle_button(self):
+        vbox = Gtk.VBox(False, 5)
+        self.vbox.pack_start(vbox, False, False, 0)
+
+        hseparator = Gtk.HSeparator()
+        hbox = Gtk.HBox(False, 0)
+
+        toggle_button = Gtk.ToggleButton("Calcular Ruta Optima")
+        toggle_button.connect("clicked", self.change_system_state, None)
+
+        hbox.pack_start(toggle_button, True, False, 0)
+        vbox.pack_start(hseparator, False, False, 0)
+        vbox.pack_start(hbox, False, False, 0)
+
+        vbox.show_all()
+
+
+    def change_system_state(self, widget, data):
+        if widget.get_active() == True:
+            widget.set_label("Desactivar Sistema")
+        else:
+            widget.set_label("Calcular Ruta Optima")
+
+
+    '''
     def key_pressed(self, widget, event, data=None):
         # Draws a random route taken from the navigation module.
         if self.init_point == None:
@@ -91,6 +239,7 @@ class GameWindow(Gtk.Window):
         self.create_and_add_route( 
             self.city.get_route_between_intersections(self.init_point, 
                                                       self.end_point))
+    '''
         
 
     def show_dialog(self, widget, data=None):
